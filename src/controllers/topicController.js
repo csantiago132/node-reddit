5c9db192169e6d969997c73395117108e561f1ed
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-10 17:40:46 -0500

    adding controllers and view for Topics

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,5 +1,13 @@
+const topicQueries = require('../db/queries.topics.js');
+
 module.exports = {
   index(request, response) {
-    response.send('TODO: list all topics');
+    topicQueries.getAllTopics((error, topics) => {
+      if (error) {
+        response.redirect(500, 'static/index');
+      } else {
+        response.render('topics/index', { topics });
+      }
+    });
   },
 };
