67b108d2c0f8fd345ac21e1e1be68baa1fcc80fa
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-09 23:42:50 -0500

    TDD on marco route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,13 +1,21 @@
-const request = require("request");
+const request = require('request');
 //const server = require("../../src/server");
-const base = "http://localhost:5000/";
+const base = 'http://localhost:5000/';
 
-describe("routes : static", () => {
-
-  describe("GET /", () => {
-    it("should return status code 200", (done) => {
+describe('routes : static', () => {
+  describe('GET /', () => {
+    it('should return status code 200', (done) => {
       request.get(base, (error, response, body) => {
         expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+
+  describe('GET /marco', () => {
+    it('should return polo', (done) => {
+      request.get(`${base}/marco`, (error, response, body) => {
+        expect(response.body).toBe('polo');
         done();
       });
     });
