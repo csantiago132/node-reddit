f28deca857de4a46bc7305cae3765e4aab5ed38a
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-09 23:58:27 -0500

    fix typo

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (reques, response) => {
+router.get('/', (request, response) => {
   response.send('Welcome to Node-Reddit');
 });
 
