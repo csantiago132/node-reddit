09102bd0002f192e0ded0dcfe112af4b44c3a62a
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-09 22:34:52 -0500

    loads the defined routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.use("/", (req, res, next) => {
-  res.send("Welcome to Bloccit")
-});
+const routeConfig = require('./config/route-config.js');
+
+routeConfig.init(app);
 
 module.exports = app;
