14d7bdf1d684f4d4428b1b73c97a35f7f69b0d12
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-09 22:36:29 -0500

    code cleanup

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,12 +1,12 @@
 const request = require("request");
-const server = require("../../src/server");
+//const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
   describe("GET /", () => {
     it("should return status code 200", (done) => {
-      request.get(base, (err, res, body) => {
+      request.get(base, (res) => {
         expect(res.statusCode).toBe(200);
         done();
       });
