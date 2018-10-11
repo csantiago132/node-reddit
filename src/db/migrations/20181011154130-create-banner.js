d2df96b08fd27afebf9f806c4b152e2cfe9e3afd
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-11 11:15:45 -0500

    fixing databases, migrated with sequelize

---
+++ src/db/migrations/20181011154130-create-banner.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Banners', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      source: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      topicId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Topic',
+          key: 'id',
+          as: 'topicId',
+        },
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Banners');
+  },
+};
