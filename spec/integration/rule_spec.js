0bd7285578dfd45cc363b8b62170b6d7174aa0e8
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-16 10:39:32 -0500

    finishing up Rule specs and views

--- spec/integration/rule_spec.js
+++ spec/integration/rule_spec.js
@@ -23,13 +23,15 @@ describe('routes : rule', () => {
     });
   });
 
-  it('should return a status code 200', (done) => {
-    request.get(base, (error, response, body) => {
-      expect(response.statusCode).toBe(200);
-      expect(error).toBeNull();
-      expect(body).toContain('Description');
-      expect(body).toContain('There is a lot of rules');
-      done();
+  describe('GET /rule', () => {
+    it('should return a status code 200', (done) => {
+      request.get(base, (error, response, body) => {
+        expect(response.statusCode).toBe(200);
+        expect(error).toBeNull();
+        expect(body).toContain('Rule');
+        expect(body).toContain('There is a lot of rules');
+        done();
+      });
     });
   });
 });
