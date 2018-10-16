f16068dfc72a26ba1ee57e68d837c6cd9d8db777
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-16 11:00:41 -0500

    adding new topic route

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -5,4 +5,6 @@ const topicController = require('../controllers/topicController');
 
 router.get('/topics', topicController.index);
 
+router.get('/topics/new', topicController.new);
+
 module.exports = router;
