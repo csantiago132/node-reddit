90ba75bd965dbd44cb6423813b55437e8c2e019d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-16 12:45:37 -0500

    adding updateMethod to a route

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -15,4 +15,6 @@ router.post('/topics/create', topicController.create);
 
 router.post('/topics/:id/destroy', topicController.destroy);
 
+router.post('/topics/:id/update', topicController.update);
+
 module.exports = router;
