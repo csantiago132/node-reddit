6826a4dddc4bce984777b60f0693e5e302fc6e19
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-16 14:56:07 -0500

    TDD ads sequelize

--- spec/integration/ads_spec.js
+++ spec/integration/ads_spec.js
@@ -1,12 +1,35 @@
 const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:5000/ads';
+const sequelize = require('../../src/db/models/index').sequelize;
+const Ads = require('../../src/db/models').Ads;
 
 describe('routes : advertisement', () => {
+  beforeEach((done) => {
+    this.ad;
+    sequelize.sync({ force: true }).then((res) => {
+      Ads.create({
+        title: 'Nike',
+        description: 'Buy Me!',
+      })
+        .then((topic) => {
+          this.ad = ad;
+          done();
+        })
+        .catch((error) => {
+          console.log(error);
+          done();
+        });
+    });
+  });
+
   describe('GET /ads', () => {
     it('should return a status code 200', (done) => {
       request.get(base, (error, response, body) => {
         expect(response.statusCode).toBe(200);
+        expect(error).toBeNull();
+        expect(body).toContain('Ads');
+        expect(body).toContain('Buy Me!');
         done();
       });
     });
