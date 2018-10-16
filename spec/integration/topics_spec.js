1fcfecd5f1c9d54c0841474043355416c61096f4
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-16 12:38:43 -0500

    TDD update topic

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -114,4 +114,26 @@ describe('routes : topics', () => {
       });
     });
   });
+
+  describe('POST /topics/:id/update', () => {
+    it('should update the topic with the given values', (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/update`,
+        form: {
+          title: 'JavaScript Frameworks',
+          description: 'There are a lot of them',
+        },
+      };
+
+      request.post(options, (error, response, body) => {
+        expect(error).toBeNull();
+        Topic.findOne({
+          where: { id: this.topic.id },
+        }).then((topic) => {
+          expect(topic.title).toBe('JavaScript Frameworks');
+          done();
+        });
+      });
+    });
+  });
 });
