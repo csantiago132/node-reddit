fc75eddcb35d0160f8b05c246b2616ee81964b99
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-17 12:20:24 -0500

    refactored topics router

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -3,18 +3,13 @@ const router = express.Router();
 
 const topicController = require('../controllers/topicController');
 
-router.get('/topics', topicController.index);
-
-router.get('/topics/new', topicController.new);
-
-router.get('/topics/:id', topicController.show);
-
-router.get('/topics/:id/edit', topicController.edit);
-
-router.post('/topics/create', topicController.create);
-
-router.post('/topics/:id/destroy', topicController.destroy);
-
-router.post('/topics/:id/update', topicController.update);
+router
+  .get('/topics', topicController.index)
+  .get('/topics/new', topicController.new)
+  .get('/topics/:id', topicController.show)
+  .get('/topics/:id/edit', topicController.edit)
+  .post('/topics/create', topicController.create)
+  .post('/topics/:id/destroy', topicController.destroy)
+  .post('/topics/:id/update', topicController.update);
 
 module.exports = router;
