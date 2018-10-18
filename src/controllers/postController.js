37c7dd548658bb3634d260c8b568297a74a4f6d2
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-18 17:54:53 -0500

    adding post feature for new post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,7 +1,22 @@
-//const postQueries = require('../../db/queries.posts.js');
+const postQueries = require('../../db/queries.posts.js');
 
 module.exports = {
   new: (request, response) => {
     response.render('posts/new', { topicId: request.params.topicId });
+  },
+
+  create: (request, response) => {
+    let newPost = {
+      title: request.body.title,
+      body: request.body.body,
+      topicId: request.params.topicId,
+    };
+    postQueries.addPost(newPost, (error, post) => {
+      if (error) {
+        response.redirect(500, '/posts/new');
+      } else {
+        response.redirect(303, `/topics/${newPost.topicId}/posts/${post.id}`);
+      }
+    });
   },
 };
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const postController = require('../controllers/postController');
 
-router.get('/topics/:topicId/posts/new', postController.new);
+router
+  .get('/topics/:topicId/posts/new', postController.new)
+  .post('/topics/:topicId/posts/create', postController.create);
 
 module.exports = router;
