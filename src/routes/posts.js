874510be4bbb621593bed24696f3c8d306434523
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-18 17:20:42 -0500

    adding new post view

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const postController = require('../controllers/postController');
 
-router.get('/posts', postController.index);
+router.get('/topics/:topicId/posts/new', postController.new);
 
 module.exports = router;
