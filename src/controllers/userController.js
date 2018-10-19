9eb01e947d9ab0e1b9d1a2464c5ea90bb72de6d2
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-19 16:25:50 -0500

    adding passport and helper functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,29 @@
+const userQueries = require('../../db/queries.users.js');
+const passport = require('passport');
+
 module.exports = {
   signUp: (request, response) => {
     response.render('users/sign_up');
+  },
+
+  create: (request, response) => {
+    let newUser = {
+      email: request.body.email,
+      password: request.body.password,
+      passwordConfirmation: request.body.passwordConfirmation,
+    };
+    // user not def at first, if needed, use:
+    // userQueries.createUser(newUser, (error, user)
+    userQueries.createUser(newUser, (error) => {
+      if (error) {
+        request.flash('error', error);
+        response.redirect('/users/sign_up');
+      } else {
+        passport.authenticate('local')(request, response, () => {
+          request.flash('notice', "You've successfully signed in!");
+          response.redirect('/');
+        });
+      }
+    });
   },
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const validation = require('./validation');
 
 const userController = require('../controllers/userController');
 
-router.get('/users/sign_up', userController.signUp);
+router
+  .get('/users/sign_up', userController.signUp)
+  .post('/users', validation.validateUsers, userController.create);
 
 module.exports = router;
