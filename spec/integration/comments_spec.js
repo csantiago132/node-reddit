c0c8c9458be7bda3bd9b96ded871cc39599d9d1d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-10-24 18:18:32 -0500

    fixing email issue that was making test fail

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -192,10 +192,10 @@ describe('routes : comments', () => {
 
       it('should not delete the comment with the associated ID of another member', (done) => {
         User.create({
-          email: 'shouldNotDeleteComment@email.com',
+          email: 'shouldNotDelete@email.com',
           password: '123456',
         }).then((user) => {
-          expect(user.email).toBe('shouldNotDeleteComment@email.com');
+          expect(user.email).toBe('shouldNotDelete@email.com');
           expect(user.id).toBe(2);
 
           request.get(
